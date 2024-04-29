8340a4b55e8df35be03b3a5f39f0fe82c63e0e5d
mharoon-dev <haroon.aghani@gmail.com>
2024-04-29 21:51:45 +0500

    home page UI

---
+++ src/Components/Categories.jsx
@@ -0,0 +1,19 @@
+import styled from "styled-components";
+import { categories } from "../data";
+import { CategoriesItem } from "./CategoriesItem";
+
+const Container = styled.div`
+    display: flex;
+    padding: 20px;
+    justify-content: space-between;
+`;
+
+export const Categories = () => {
+  return (
+      <Container>
+        {categories.map((item) => (
+          <CategoriesItem item={item} key={item.id} />
+        ))}
+      </Container>
+  )
+};
